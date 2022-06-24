c895e2c5f363eb2f15048e4985dee7aad7c9ba90
Retr0A <102794452+Retr0Alt@users.noreply.github.com>
2022-06-24 15:01:00 +0300

    ...

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,7 +9,7 @@ function Home() {
                     Your browser does not support HTML5 video.
             </video>
 
-            <h1 className="w3-container w3-center w3-animate-bottom">Indie game developer, Youtube</h1>
+            <h1 className="w3-container w3-center w3-animate-bottom">Game developer, Youtube</h1>
             <h2 className="w3-container w3-center w3-animate-bottom">if you want to learn more about me, go to about page.</h2>
 
             <h5 className="copyright-text">©Retr0A. All rights reserved.</h5>
